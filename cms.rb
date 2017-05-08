b108dbca3a26a602bd437f195d9e3fa5ddce4c8f
Daniel Vopalecky <daniel.vopalecky@gmail.com>
2017-05-08 12:47:35 +0200

    Change single to double quotes on most places and fix few rubicop issues

--- cms.rb
+++ cms.rb
@@ -1,21 +1,21 @@
 # File based CMS
 
-require 'sinatra'
-require 'sinatra/reloader'
-require 'tilt/erubis'
-require 'redcarpet'
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+require "redcarpet"
 
 configure do
   enable :sessions
-  set :session_secret, 'secret'
+  set :session_secret, "secret"
   set :erb, escape_html: true
 end
 
 def data_path
-  if ENV['RACK_ENV'] == 'test'
-    File.expand_path('../test/data', __FILE__)
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
   else
-    File.expand_path('../data', __FILE__)
+    File.expand_path("../data", __FILE__)
   end
 end
 
@@ -27,13 +27,13 @@ end
 # returns validation boolean status and message
 def validate_filename(filename)
   if filename.empty?
-    [false, 'A name is required']
+    [false, "A name is required"]
   elsif /[^\w\.]/ =~ filename
-    [false, 'Name must contain only alphanumeric chars or . or _']
-  elsif !['.txt', '.md'].include?(File.extname(filename))
-    [false, 'Document must have .md or .txt extensions']
+    [false, "Name must contain only alphanumeric chars or . or _"]
+  elsif ![".txt", ".md"].include?(File.extname(filename))
+    [false, "Document must have .md or .txt extensions"]
   else
-    [true, '']
+    [true, ""]
   end
 end
 
@@ -42,30 +42,29 @@ def user_signed_in?
 end
 
 def redirect_if_signed_out
-  unless user_signed_in?
-    session[:message] = "You must be signed in to do that."
-    redirect '/'
-  end
+  return if user_signed_in?
+  session[:message] = "You must be signed in to do that."
+  redirect "/"
 end
 
 # Show index
-get '/' do
-  pattern = File.join(data_path, '*')
+get "/" do
+  pattern = File.join(data_path, "*")
   @files = Dir[pattern].select { |path| File.file?(path) }
   @files = @files.map! { |file| File.basename(file) }
   erb :index
 end
 
 # Create new document
-post '/' do
+post "/" do
   redirect_if_signed_out
   name = params[:name].strip
   validation_status, validation_msg = validate_filename(name)
   if validation_status
     path = File.join(data_path, name)
-    File.write(path, '')
+    File.write(path, "")
     session[:message] = "#{name} has been created"
-    redirect '/'
+    redirect "/"
   else
     session[:message] = validation_msg
     status 422
@@ -74,61 +73,59 @@ post '/' do
 end
 
 # Show sign in page
-get '/users/signin' do
-    erb :sign_in
+get "/users/signin" do
+  erb :sign_in
 end
 
 # Sign in
-post '/users/signin' do
+post "/users/signin" do
   if user_signed_in?
     session[:message] = "You're already signed in"
-    redirect '/'
+    redirect "/"
+  elsif params[:username] == "admin" && params[:password] == "secret"
+    session[:signed_in_user] = params[:username]
+    session[:message] = "Welcome!"
+    redirect "/"
   else
-    if params[:username] == 'admin' && params[:password] == 'secret'
-      session[:signed_in_user] = params[:username]
-      session[:message] = 'Welcome!'
-      redirect '/'
-    else
-      session[:message] = 'Invalid credentials'
-      @username = params[:username]
-      status 422
-      erb :sign_in
-    end
+    session[:message] = "Invalid credentials"
+    @username = params[:username]
+    status 422
+    erb :sign_in
   end
 end
 
 # Sign out
-post '/users/signout' do
+post "/users/signout" do
   session.delete :signed_in_user
-  session[:message] = 'You have been signed out.'
-  redirect '/'
+  session[:message] = "You have been signed out."
+  redirect "/"
 end
 
 # Show form for new document
-get '/new' do
+get "/new" do
   redirect_if_signed_out
   erb :new
 end
 
 # Show document
-get '/:filename' do
+get "/:filename" do
   path = File.join(data_path, params[:filename])
   if File.file?(path)
     content = File.read(path)
-    if File.extname(path) == '.md'
+    if File.extname(path) == ".md"
       render_markdown(content)
     else
-      headers['Content-Type'] = 'text/plain;charset=utf-8'
+      headers["Content-Type"] = "text/plain;charset=utf-8"
       content
     end
   else
     session[:message] = "#{params[:filename]} doesn't exist."
-    redirect '/'
+    redirect "/"
   end
 end
 
 # Submit edits of document
-post '/:filename' do
+post "/:filename" do
   redirect_if_signed_out
   path = File.join(data_path, params[:filename])
   if File.file?(path)
@@ -138,11 +135,11 @@ post '/:filename' do
     session[:message] = "Can't edit non-existing document #{params[:filename]}"
   end
 
-  redirect '/'
+  redirect "/"
 end
 
 # Show form for editing document
-get '/:filename/edit' do
+get "/:filename/edit" do
   redirect_if_signed_out
   @filename = params[:filename]
   path = File.join(data_path, @filename)
@@ -151,12 +148,12 @@ get '/:filename/edit' do
     erb :edit
   else
     session[:message] = "Can't edit non-existing document #{@filename}"
-    redirect '/'
+    redirect "/"
   end
 end
 
 # Delete document
-post '/:filename/delete' do
+post "/:filename/delete" do
   redirect_if_signed_out
   @filename = params[:filename]
   path = File.join(data_path, @filename)
@@ -166,5 +163,5 @@ post '/:filename/delete' do
   else
     session[:message] = "Can't delete non-existing document #{@filename}."
   end
-  redirect '/'
+  redirect "/"
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -1,10 +1,10 @@
-ENV['RACK_ENV'] = 'test'
+ENV["RACK_ENV"] = "test"
 
-require 'minitest/autorun'
-require 'rack/test'
-require 'fileutils'
+require "minitest/autorun"
+require "rack/test"
+require "fileutils"
 
-require_relative '../cms'
+require_relative "../cms"
 
 class CMSTest < Minitest::Test
   include Rack::Test::Methods
@@ -18,13 +18,13 @@ class CMSTest < Minitest::Test
   end
 
   def teardown
-    FileUtils.rm Dir[File.join(data_path,'*.md')]
-    FileUtils.rm Dir[File.join(data_path,'*.txt')]
+    FileUtils.rm Dir[File.join(data_path, "*.md")]
+    FileUtils.rm Dir[File.join(data_path, "*.txt")]
     FileUtils.rmdir data_path
   end
 
-  def create_document(name, content='')
-    File.open(File.join(data_path, name), 'w') do |file|
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
       file.write(content)
     end
   end
@@ -34,216 +34,218 @@ class CMSTest < Minitest::Test
   end
 
   def session
-    last_request.env['rack.session']
+    last_request.env["rack.session"]
   end
 
   def test_index_signed_off
-    get '/'
+    get "/"
 
     assert_equal 200, last_response.status
     assert_match "You're signed off", last_response.body
-    assert_match '<button', last_response.body
-    assert_match 'href="/users/signin"', last_response.body
+    assert_match "<button", last_response.body
+    assert_match %q(href="/users/signin"), last_response.body
   end
 
   def test_index
-    create_document 'about.md'
-    create_document 'changes.txt'
+    create_document "about.md"
+    create_document "changes.txt"
 
-    get '/', {}, admin_session
+    get "/", {}, admin_session
 
     assert_equal 200, last_response.status
-    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
-    assert_match 'about.md', last_response.body
-    assert_match 'changes.txt', last_response.body
-    assert_match '<a href="/about.md/edit">Edit</a>', last_response.body
-    assert_match '<a href="/new">New document</a>', last_response.body
-    assert_match '<form action="/changes.txt/delete"', last_response.body
-    assert_match '<button', last_response.body
-    assert_match 'Signed in as admin', last_response.body
-    assert_match 'Sign out', last_response.body
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_match "about.md", last_response.body
+    assert_match "changes.txt", last_response.body
+    assert_match %q(<a href="/about.md/edit">Edit</a>), last_response.body
+    assert_match %q(<a href="/new">New document</a>), last_response.body
+    assert_match %q(<form action="/changes.txt/delete"), last_response.body
+    assert_match "<button", last_response.body
+    assert_match "Signed in as admin", last_response.body
+    assert_match "Sign out", last_response.body
   end
 
   def test_sign_in_form
-    get '/users/signin'
+    get "/users/signin"
 
     assert_equal 200, last_response.status
-    assert_match 'Username', last_response.body
-    assert_match 'Password', last_response.body
-    assert_match '<form', last_response.body
-    assert_match '<button', last_response.body
+    assert_match "Username", last_response.body
+    assert_match "Password", last_response.body
+    assert_match "<form", last_response.body
+    assert_match "<button", last_response.body
   end
 
   def test_sign_in_valid_credentials
-    post '/users/signin', username: 'admin', password: 'secret'
+    post "/users/signin", username: "admin", password: "secret"
 
     assert_equal 302, last_response.status
-    assert_equal 'Welcome!', session[:message]
-    assert_equal 'admin', session[:signed_in_user]
+    assert_equal "Welcome!", session[:message]
+    assert_equal "admin", session[:signed_in_user]
   end
 
   def test_sign_in_invalid_credentials
-    post '/users/signin', username: 'test', password: 'test'
+    post "/users/signin", username: "test", password: "test"
 
     assert_equal 422, last_response.status
-    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
-    assert_match 'Invalid credentials', last_response.body
-    assert_match '<input type="text" name="username" value="test">',
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_match "Invalid credentials", last_response.body
+    assert_match %q(<input type="text" name="username" value="test">),
                  last_response.body
     assert_nil session[:user_signed_in]
   end
 
   def test_sign_out
-    post '/users/signout'
+    post "/users/signout"
 
     assert_equal 302, last_response.status
-    assert_equal 'You have been signed out.', session[:message]
+    assert_equal "You have been signed out.", session[:message]
     assert_nil session[:user_signed_in]
 
-    get last_response['Location']
+    get last_response["Location"]
     assert_match "You're signed off", last_response.body
   end
 
   def test_view_new_document_form
-    get '/new', {}, admin_session
+    get "/new", {}, admin_session
 
     assert_equal 200, last_response.status
-    assert_match 'Add a new document', last_response.body
-    assert_match '<input', last_response.body
-    assert_match '<form action="/"', last_response.body
+    assert_match "Add a new document", last_response.body
+    assert_match "<input", last_response.body
+    assert_match %q(<form action="/"), last_response.body
   end
 
   def test_view_new_document_form_signed_out
-    get '/new'
+    get "/new"
     assert_equal 302, last_response.status
     assert_equal "You must be signed in to do that.", session[:message]
   end
 
   def test_create_new_document
-    post '/', { name: 'newfile.txt' }, admin_session
+    post "/", { name: "newfile.txt" }, admin_session
 
     assert_equal 302, last_response.status
     assert_equal "newfile.txt has been created", session[:message]
 
-    get '/'
-    assert_match 'newfile.txt', last_response.body
+    get "/"
+    assert_match "newfile.txt", last_response.body
 
-    get '/newfile.txt'
+    get "/newfile.txt"
     assert_equal 200, last_response.status
-    assert_equal '', last_response.body
+    assert_equal "", last_response.body
   end
 
   def test_create_new_document_signed_out
-    post '/', name: 'newfile.txt'
+    post "/", name: "newfile.txt"
 
     assert_equal 302, last_response.status
     assert_equal "You must be signed in to do that.", session[:message]
 
-    assert_equal false, File.file?(File.join(data_path, 'newfile.txt'))
+    assert_equal false, File.file?(File.join(data_path, "newfile.txt"))
   end
 
   def test_create_new_document_invalid_path
-    post '/', { name: '../hello.txt' }, admin_session
+    post "/", { name: "../hello.txt" }, admin_session
 
     assert_equal 422, last_response.status
-    assert_match 'Name must contain only alphanumeric chars or . or _', last_response.body
+    assert_match "Name must contain only alphanumeric chars or . or _",
+                 last_response.body
   end
 
   def test_create_new_document_invalid_extension
-    post '/', { name: 'hello' }, admin_session
+    post "/", { name: "hello" }, admin_session
 
     assert_equal 422, last_response.status
-    assert_match 'Document must have .md or .txt extensions', last_response.body
+    assert_match "Document must have .md or .txt extensions", last_response.body
   end
 
   def test_create_new_document_empty_name
-    get '/', {}, admin_session
-    post '/', name: ''
+    get "/", {}, admin_session
+    post "/", name: ""
 
     assert_equal 422, last_response.status
-    assert_match 'A name is required', last_response.body
+    assert_match "A name is required", last_response.body
   end
 
   def test_viewing_text_document
     content = "History of the World\nIn the beginning was the Word.\n"
-    create_document 'history.txt', content
+    create_document "history.txt", content
 
-    get '/history.txt'
+    get "/history.txt"
 
     assert_equal 200, last_response.status
-    assert_equal 'text/plain;charset=utf-8', last_response['Content-Type']
+    assert_equal "text/plain;charset=utf-8", last_response["Content-Type"]
     assert_equal content, last_response.body
   end
 
   def test_viewing_markup_document
     content = "# Jan Amos Komenský\n\n## Basic info\n"
-    create_document 'about.md', content
+    create_document "about.md", content
 
-    get '/about.md'
+    get "/about.md"
 
     assert_equal 200, last_response.status
-    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
-    assert_match '<h1>Jan Amos Komenský</h1>', last_response.body
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_match "<h1>Jan Amos Komenský</h1>", last_response.body
   end
 
   def test_view_edit_form
     content = "# Jan Amos Komensky\n\n## Basic info\n"
-    create_document 'about.md', content
+    create_document "about.md", content
 
-    get '/about.md/edit', {}, admin_session
+    get "/about.md/edit", {}, admin_session
 
     assert_equal 200, last_response.status
-    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
-    assert_match /<textarea.*>.*Jan Amos/, last_response.body
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_match(/<textarea.*>.*Jan Amos/, last_response.body)
     assert_match "<button type=\"submit\"", last_response.body
   end
 
   def test_view_edit_form_signed_out
-    create_document 'about.md', "# something"
-    get '/about.md/edit'
+    create_document "about.md", "# something"
+    get "/about.md/edit"
 
     assert_equal 302, last_response.status
     assert_equal "You must be signed in to do that.", session[:message]
   end
 
   def test_view_edit_form_nonexisting_document
-    get '/about.md/edit', {}, admin_session
+    get "/about.md/edit", {}, admin_session
 
     assert_equal 302, last_response.status
     assert_equal "Can't edit non-existing document about.md", session[:message]
   end
 
   def test_updating_existing_document
-    create_document 'changes.txt', 'something random'
-    post '/changes.txt', { content: 'new content' }, admin_session
+    create_document "changes.txt", "something random"
+    post "/changes.txt", { content: "new content" }, admin_session
 
     assert_equal 302, last_response.status
     assert_equal "changes.txt has been updated.", session[:message]
 
-    get '/changes.txt'
+    get "/changes.txt"
     assert_equal 200, last_response.status
-    assert_match 'new content', last_response.body
+    assert_match "new content", last_response.body
   end
 
   def test_updating_existing_document_signed_off
-    create_document 'changes.txt', 'old'
-    post '/changes.txt', content: 'new content'
+    create_document "changes.txt", "old"
+    post "/changes.txt", content: "new content"
 
     assert_equal 302, last_response.status
     assert_equal "You must be signed in to do that.", session[:message]
 
-    assert_equal 'old', File.read(File.join(data_path, 'changes.txt'))
+    assert_equal "old", File.read(File.join(data_path, "changes.txt"))
   end
 
   def test_updating_nonexisting_document
-    post '/changes.txt', { content: 'new content' }, admin_session
+    post "/changes.txt", { content: "new content" }, admin_session
 
     assert_equal 302, last_response.status
-    assert_equal "Can't edit non-existing document changes.txt", session[:message]
+    assert_equal "Can't edit non-existing document changes.txt",
+                 session[:message]
   end
 
   def test_nonexisting_document
-    get '/notafile.txt'
+    get "/notafile.txt"
     assert_equal 302, last_response.status
     assert_equal "notafile.txt doesn't exist.", session[:message]
 
@@ -252,31 +254,32 @@ class CMSTest < Minitest::Test
   end
 
   def test_delete_document
-    create_document 'file.txt', 'something'
-    post '/file.txt/delete', {}, admin_session
+    create_document "file.txt", "something"
+    post "/file.txt/delete", {}, admin_session
 
     assert_equal 302, last_response.status
     assert_equal "file.txt deleted successfully.", session[:message]
-    assert_equal false, File.file?(File.join(data_path, 'file.txt'))
+    assert_equal false, File.file?(File.join(data_path, "file.txt"))
 
     get last_response["Location"]
     assert_nil session[:message]
   end
 
   def test_delete_document_singed_off
-    create_document 'file.txt', 'something'
+    create_document "file.txt", "something"
 
-    post '/file.txt/delete'
+    post "/file.txt/delete"
     assert_equal 302, last_response.status
     assert_equal "You must be signed in to do that.", session[:message]
-    assert_equal true, File.file?(File.join(data_path, 'file.txt'))
+    assert_equal true, File.file?(File.join(data_path, "file.txt"))
   end
 
   def test_delete_nonnexisting_document
-    post '/file.txt/delete', {}, admin_session
+    post "/file.txt/delete", {}, admin_session
 
     assert_equal 302, last_response.status
-    assert_equal "Can't delete non-existing document file.txt.", session[:message]
-    assert_equal false, File.file?(File.join(data_path, 'file.txt'))
+    assert_equal "Can't delete non-existing document file.txt.",
+                 session[:message]
+    assert_equal false, File.file?(File.join(data_path, "file.txt"))
   end
 end
